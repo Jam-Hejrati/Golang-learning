f45ab734ea8fadc161941ab1071bceba3a30f290
Jam Hejrati <jamhejratipro@gmail.com>
2022-09-13 23:25:17 +0430

    operators

--- chapter-1/main.go
+++ chapter-1/main.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// Hello World
 func main(){
 	fmt.Println("Hello World")
 }
---
+++ chapter-2/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+// Operators
+func main(){
+	fmt.Println("1 + 1 =" , 1+1)
+}
