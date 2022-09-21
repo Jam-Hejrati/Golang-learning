859513727f50bfbae9a5f464b7427045bbef42a5
Jam Hejrati <jamhejratipro@gmail.com>
2022-09-21 15:20:57 +0430

    for loop & if statement

--- chapter-4/main.go
+++ chapter-4/main.go
@@ -2,31 +2,15 @@ package main
 
 import "fmt"
 
-// Variables
-var globallyVariable = "this is a global variable" // type 'string' could omit so omitted :)
+// If statements
+// for loop
 
 func main() {
-	var x string = "Hello World"
-	fmt.Println(x)
-	var y string
-	y = "Hello Jam"
-	fmt.Println(y)
-	y = "Hello Jam 2"
-	fmt.Println(y)
-	y = y + " Second"
-	fmt.Println(y)
-	z := " this is a string"
-	w := 45
-	fmt.Println(z, w)
-	fmt.Println(globallyVariable)
-
-	const str string = "first" // can't change the value anymore
-	fmt.Println(str)
-
-	var (
-		a = 5
-		b = 10
-		c = 15
-	)
-	fmt.Println(a + b + c)
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			fmt.Println("number", i, "even")
+		} else {
+			fmt.Println("number", i, "odd")
+		}
+	}
 }
